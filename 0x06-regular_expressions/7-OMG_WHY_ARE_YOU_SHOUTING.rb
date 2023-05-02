2066fdb37cb0f0603a6290ff5bc673e9aea78b69
Bacia22 <adolfobacia@yahoo.com>
2023-05-02 14:40:07 -0700

    7-OMG_WHY_ARE_YOU_SHOUTING.rb

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# This ruby script use regerx to match only capital letters
-puts ARGV[0].scan(/[A-Z/).join
+# This ruby script use regex to match only capital letters
+puts ARGV[0].scan(/[A-Z]/).join
